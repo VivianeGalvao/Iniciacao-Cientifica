a03591e05ab2402b1382fd29d2295db4c05da22f
VivianeGalvao <VivianeGalvao@e4a8a417-171d-47c4-8793-c9270230c0fa>
2015-02-26 14:09:57 +0000

    essage

---
+++ references/Implementacoes/TestesFinais2/Avaliation_Function.h
@@ -0,0 +1,35 @@
+#ifndef AVALIATION_FUNCTION_H_INCLUDED
+#define AVALIATION_FUNCTION_H_INCLUDED
+
+
+double Compute_Function(double* x, int size, int num_func);
+void Lower_Bounds(int num_fun, int size, double *lb);
+void Upper_Bounds(int num_fun, int size, double *ub);
+int Number_Evaluations(int number_function);
+double Sphere_function(double* x, int size);
+double f02(double* x, int size);
+double f03(double* x, int size);
+double f04(double* x, int size);
+double f05(double* x, int size);
+double f06(double* x, int size);
+double f07(double* x, int size);
+double f08(double* x, int size);
+double f09(double* x, int size);
+double f10(double* x, int size);
+double f11(double* x, int size);
+double f12(double* x, int size);
+double f13(double* x, int size);
+double f14(double* x, int size);
+double f15(double* x, int size);
+double f16(double* x, int size);
+double f17(double* x, int size);
+double f18(double* x, int size);
+double f19(double* x, int size);
+double f20(double* x, int size);
+double f21(double* x, int size);
+double f22(double* x, int size);
+
+
+
+
+#endif // AVALIATION_FUNCTION_H_INCLUDED
