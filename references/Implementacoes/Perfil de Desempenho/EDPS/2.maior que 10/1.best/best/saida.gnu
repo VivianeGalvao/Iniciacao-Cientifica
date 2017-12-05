2ab522d3a893571aaa034ce5d46d04fa34f3887d
vivianegalvao <vivianegalvao@e4a8a417-171d-47c4-8793-c9270230c0fa>
2017-12-05 16:58:04 +0000

    Atualizacao Perfil de Desempenho/EDPS

--- references/Implementacoes/Perfil de Desempenho/EDPS/2.maior que 10/1.best/best/saida.gnu
+++ references/Implementacoes/Perfil de Desempenho/EDPS/2.maior que 10/1.best/best/saida.gnu
@@ -1,22 +1,45 @@
-set logscale x
+#set style data lines
+#set ylabel '{/Symbol r}({/Symbol t})'
+#set xlabel '{/Symbol t}'
+#set term postscript eps enhanced color
+#set output 'saida.gnu.eps'
+#set key right bottom
+#set xrange [1:7.102743491140409]
+#set yrange [0:1.01]
+
+set size 1,1
+#set size 0.3,1
 set style data lines
-set ylabel '{/Symbol r}({/Symbol t})'
-set xlabel '{/Symbol t}'
-set term postscript eps enhanced color
-set output 'saida2.eps'
-set key right bottom
-set xrange [1:7.102743491140409]
-set yrange [0:1.01]
-plot 'out_N1_saida.gnu.dat' u ($1):($2)  title 'DE/best/1' lw 10 lt 0 , 'out_N2_saida.gnu.dat' u ($1):($2)  title 'DE/best/1+BP' lw 10 lt 1 , 'out_N3_saida.gnu.dat' u ($1):($2)  title 'DE/best/1+BPM' lw 10 lt 2
-set output 'saida.gnu_bar.eps'
 unset key
-set ylabel 'Area'
-set xlabel 'Algoritmo'
-set autoscale
-unset logscale x
-set boxwidth 0.8
-set style fill solid border -1
-set xrange [1:7]
-set yrange [0.5:1.005]
-set xtics ('DE/best/1' 2, 'DE/best/1+BP' 4, 'DE/best/1+BPM' 6)
-plot 'out_saida.gnu.dat' usi (2):1 title 'N1' w boxes lt 0, 'out_saida.gnu.dat' usi (4):2 title 'N2' w boxes lt 1, 'out_saida.gnu.dat' usi (6):3 title 'N3' w boxes lt 2
+set key right bottom
+set key font ",20"
+set logscale x
+#set ylabel '{/Symbol r}({/Symbol t})'
+set xlabel 'log({/Symbol t})' font ",15"
+#set xlabel '{/Symbol t}'
+set term postscript eps enhanced color font ",15"
+#set output 'saida.gnu.eps'
+set output 'saida2.eps'
+#set xrange [1:104119.00499999999]
+set xrange [1.03:100]
+#set xrange [1:1.03]
+#set xtics 1.03
+#set yrange [0:1.01]
+set yrange [0:1.01]
+unset ytics
+
+plot 'out_N1_saida.gnu.dat' u ($1):($2)  title 'DE/best' lw 10 lt 0 , 'out_N2_saida.gnu.dat' u ($1):($2)  title 'DE/best+PS' lw 10 lt 1 , 'out_N3_saida.gnu.dat' u ($1):($2)  title 'DE/best+PS*' lw 10 lt 2
+
+
+#set output 'saida.gnu_bar.eps'
+#unset key
+#set ylabel 'Area'
+#set xlabel 'Algorithm'
+#set autoscale
+#unset logscale x
+#set boxwidth 0.8
+#set style fill solid border -1
+#set xrange [1:7]
+#set yrange [0.5:1.005]
+#set xtics ('N1' 2, 'N2' 4, 'N3' 6)
+#plot 'out_saida.gnu.dat' usi (2):1 title 'N1' w boxes lt 0, 'out_saida.gnu.dat' usi (4):2 title 'N2' w boxes lt 1, 'out_saida.gnu.dat' usi (6):3 title 'N3' w boxes lt 2
---
+++ references/Implementacoes/Perfil de Desempenho/EDPS/2.maior que 10/1.best/original/Estrategias.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iomanip>
+#include "Estrategias.h"
+#include "Individuo.h"
+#include <time.h>
+#include <string.h>
+#include <fstream>
+#include <cmath>
+
+#define INF 1E+21
+
+using namespace std;
+
+int func_eval=0;
+double delta;
+int success_direction = -1;
+
+double random_generate(){
+    return ((rand())/(RAND_MAX+1.0));
+}
+
+void Exploratory_Moves(double (*objfun)(double *), Individuo *sample, int size, double* lb, double* ub){
+    if(sample){
+        int directions = 2*size;
+        double **points = new double*[directions];
+        double aux;
+        for(int i=0; i<directions; i++){
+            points[i] = new double[size];
+        }
+
+        bool fail;
+        bool exit=false;
+        for(int i=0; i<directions; i++){
+             for(int j=0; j<size; j++){
+                if(i<size){
+                    if(i == j){
+                        points[i][j] = sample->posicao[j] + delta;
+                    }
+                    else{
+                        points[i][j] = sample->posicao[j];
+                    }
+                }
+                else{
+                    if((i - j) == size){
+                        points[i][j] = sample->posicao[j] - delta;
+                    }
+                    else{
+                        points[i][j] = sample->posicao[j];
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<directions; i++){
+            fail = false;
+            for(int j=0; j<size; j++){
+                if(lb && ub){
+                    if(points[i][j] < lb[j] || points[i][j] > ub[j]){
+                        fail = true;
+                        break;
+                    }
+                }
+            }
+            if(!fail){
+                aux = objfun(points[i]); func_eval++;
+                if(sample->valor > aux){
+                    sample->valor = aux;
+                    for(int j=0; j<size; j++){
+                        sample->posicao[j] = points[i][j];
+                    }
+                    if(success_direction == i){
+                        delta = 2*delta;
+                    }
+                    else{
+                        success_direction = i;
+                    }
+                    exit = true;
+                }
+            }
+            if(exit)break;
+        }
+
+        if(!exit){
+            success_direction = -1;
+            delta = 0.5*delta;
+        }
+
+        for(int i=0; i<directions; i++){
+            delete []points[i];
+        }
+        delete []points;
+
+    }
+    else{
+        cout<<"Ponto nulo. Com amor, Pattern Search"<<endl;
+    }
+}
+
+double EvolucaoDiferencial(double (*objfun)(double*), double fator_ponderacao, double cr, int dimension, int num_individuos, int seed, double *lb, double *ub){
+
+    func_eval=0;
+    srand(seed);
+
+    Individuo **pop = new Individuo*[num_individuos];
+    Individuo *best = new Individuo(dimension);
+
+    for(int i=0; i<num_individuos; i++){
+        pop[i] = new Individuo(dimension);
+
+        for(int j=0; j<dimension; j++){
+            if(lb && ub){
+                pop[i]->posicao[j] = (ub[j]-lb[j])*random_generate() + lb[j];
+            }
+            else{
+                pop[i]->posicao[j] = random_generate();
+            }
+        }
+    }
+
+    for(int i=0; i<num_individuos; i++){
+        pop[i]->valor = objfun(pop[i]->posicao);
+        func_eval++;
+    }
+
+    int ibest = 0;
+    best->valor = pop[0]->valor;
+    for(int i=0; i<dimension; i++){
+        best->posicao[i] = pop[0]->posicao[i];
+    }
+    for(int i=1; i<num_individuos; i++){
+        if(best->valor > pop[i]->valor){
+            ibest = i;
+            best->valor = pop[i]->valor;
+        }
+    }
+    for(int i=0; i<dimension; i++){
+        best->posicao[i] = pop[ibest]->posicao[i];
+    }
+
+//    int parada = Number_Evaluations(num_func);
+    int parada = 2000;
+
+    while(func_eval < parada){
+        int success = 0;
+
+        int alpha, beta, gamma;
+        double mutado[num_individuos][dimension];
+        Individuo **tentativa = new Individuo*[num_individuos];
+        for(int i=0; i<num_individuos; i++){
+            tentativa[i] = new Individuo(dimension);
+        }
+
+        //mutação
+        for(int i=0; i<num_individuos; i++){
+
+            alpha = rand()%num_individuos;
+            beta = rand()%num_individuos;
+            gamma = rand()%num_individuos;
+
+            while(alpha == i){
+                alpha = rand()%num_individuos;
+            }
+            while(beta == alpha || beta == i){
+                beta = rand()%num_individuos;
+            }
+//            while(gamma == beta || gamma == alpha || gamma == i){
+//                gamma = rand()%num_individuos;
+//            }
+
+            for(int j=0; j<dimension; j++){
+                mutado[i][j] = best->posicao[j] + fator_ponderacao*(pop[alpha]->posicao[j] - pop[beta]->posicao[j]);
+
+                if(mutado[i][j] > ub[j]){
+                    mutado[i][j] = ub[j];
+                }
+                if(mutado[i][j] < lb[j]){
+                    mutado[i][j] = lb[j];
+                }
+            }
+
+        }
+
+        //cruzamento
+        double r;
+        for(int i=0; i<num_individuos; i++){
+           for(int j=0; j<dimension; j++){
+                r = random_generate();
+                if(r <= cr){
+                    tentativa[i]->posicao[j] = mutado[i][j];
+                }
+                else{
+                    tentativa[i]->posicao[j] = pop[i]->posicao[j];
+                }
+            }
+        }
+
+        //selecao
+        for(int i=0; i<num_individuos; i++){
+
+            tentativa[i]->valor = objfun(tentativa[i]->posicao);
+            func_eval++;
+            if(tentativa[i]->valor < pop[i]->valor){
+                pop[i]->valor = tentativa[i]->valor;
+                for(int j=0; j<dimension; j++){
+                    pop[i]->posicao[j] = tentativa[i]->posicao[j];
+                }
+                if(pop[i]->valor < best->valor){
+                    success = 1;
+                    best->valor = pop[i]->valor;
+                    for(int j=0; j<dimension; j++){
+                        best->posicao[j] = pop[i]->posicao[j];
+                    }
+                }
+            }
+        }
+
+//         if(success == 0){
+//             Exploratory_Moves(objfun, best, dimension, lb, ub);
+//         }
+
+        for(int i=0; i<num_individuos; i++){
+            delete tentativa[i];
+        }
+        delete tentativa;
+    }
+
+//    cout<<";"<<setprecision(10)<<best->valor;
+//    cout<<setprecision(10)<<best->valor;
+
+    double aux = best->valor;
+    delete best;
+    for(int i=0; i<num_individuos; i++){
+        delete pop[i];
+    }
+    delete pop;
+
+    return aux;
+}
