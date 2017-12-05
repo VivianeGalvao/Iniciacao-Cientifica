18f467f4e424a8926249449d533dac116ac169b5
vivianegalvao <vivianegalvao@e4a8a417-171d-47c4-8793-c9270230c0fa>
2017-12-05 16:47:39 +0000

    Atualização Perfil de Desempenho/EDPS/1.todos/

---
+++ references/Implementacoes/Perfil de Desempenho/EDPS/1.todos/2.rand/BLM/Estrategias.h
@@ -0,0 +1,6 @@
+#ifndef ESTRATEGIAS_H_INCLUDED
+#define ESTRATEGIAS_H_INCLUDED
+
+double EvolucaoDiferencialBL(double (*objfun)(double*), double fator_ponderacao, double cr, int dimension, int num_individuos, int seed, double *lb, double *ub, double delta_inicial);
+
+#endif // ESTRATEGIAS_H_INCLUDED
